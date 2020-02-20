ace441dcc2a681bb3dbb821851107c3d9e88fcaa
Yiheng (NUC) <development.henryquan@gmail.com>
2020-02-20 23:07:59 +1100

    created homepage

---
+++ habit/lib/src/ui/pages/HomePage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+/// HomePage class
+class HomePage extends StatelessWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HomePage')
+      ),
+      body: Container(),
+    );
+  }
+}
