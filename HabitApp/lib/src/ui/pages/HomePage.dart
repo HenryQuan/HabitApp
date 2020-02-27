3756ada595d348ebd6fa05d9a862403765920c71
Yiheng (NUC) <development.henryquan@gmail.com>
2020-02-27 21:37:19 +1100

    ThemedWidget doesn't work in HomePage

--- HabitApp/lib/src/ui/pages/HomePage.dart
+++ HabitApp/lib/src/ui/pages/HomePage.dart
@@ -3,9 +3,15 @@ import 'package:HabitApp/src/ui/widgets/ThemedWidget.dart';
 import 'package:flutter/material.dart';
 
 /// HomePage class
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
 
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final isDarkMode = Utils.isDarkTheme(context);
